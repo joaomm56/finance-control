47486649cc850e12e3dd2d5f9b6b53dc761b70ec
joaomm56 <joao.pmmrta@gmail.com>
2026-02-18 15:45:05 +0000

    refactor: clean up supabase client initialization and imports

--- app/database.py
+++ app/database.py
@@ -1,10 +1,9 @@
 import os
-from supabase import create_client, Client
+import re
 from dotenv import load_dotenv
+from supabase import create_client, Client
 
 load_dotenv()
-
-url: str = os.getenv("SUPABASE_URL")
-key: str = os.getenv("SUPABASE_KEY")
-
-supabase: Client = create_client(url, key)
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key) 
