3b5a3dd3a104efc90653496959bcaec0dac8e167
JoaoM562 <joao.pmmurta@gmail.com>
2026-02-19 12:11:56 +0000

    fix: update API URL to production

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://finance-control-axh4.onrender.com',
   headers: { 'Content-Type': 'application/json' }
 })
 
