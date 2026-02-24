bef9c3635b43ed05d6aad545e11daf7488af4547
JoaoM562 <joao.pmmurta@gmail.com>
2026-02-24 20:15:45 +0000

    fix: include all node_modules in commonjs transform

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  },
-
   build: {
     commonjsOptions: {
-      include: [/clsx/, /recharts/, /node_modules/]
+      include: [/node_modules/]
     }
   }
 })
