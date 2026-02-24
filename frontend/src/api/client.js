3598b59cf8ea3709f4e09216087f310c4dcb2c05
joaomm56 <joao.pmmrta@gmail.com>
2026-02-24 10:43:29 +0000

    feat: update API URL to railway and CORS to netlify

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://finance-control-axh4.onrender.com',
+  baseURL: 'https://finance-control-production.up.railway.app',
   headers: { 'Content-Type': 'application/json' }
 })
 
