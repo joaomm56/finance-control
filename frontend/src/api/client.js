57936b6b8bfd9a0912d5513bbe803f7854e67816
joaomm56 <joao.pmmrta@gmail.com>
2026-03-02 15:14:34 +0000

    new backend deploy

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://finance-control-production.up.railway.app',
+  baseURL: 'https://finance-control-cj7w.onrender.com/',
   headers: { 'Content-Type': 'application/json' }
 })
 
